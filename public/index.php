f632435681332314c8e06a631de46285fbb8022a
Remembers5 <35206632+Remembers5@users.noreply.github.com>
2018-03-15 10:04:35 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
+   
 use LINE\LINEBot\EchoBot\Dependency;
 use LINE\LINEBot\EchoBot\Route;
 use LINE\LINEBot\EchoBot\Setting;
